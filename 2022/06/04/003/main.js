f7916ff84e68021f2163cd74b9c1ac6d2e8d7b32
cogroid <89304967+cogroid@users.noreply.github.com>
2022-06-04 15:26:00 -0400

    Update main.js

--- 2022/06/04/003/main.js
+++ 2022/06/04/003/main.js
@@ -1,13 +1,20 @@
 function __exec__(data) {
 	eval(use('word'));
+	eval(use('variable'));
+  
 	var vwd = word('dream');
 	log().info(vwd.toShortString());
+	var vvr = variable('money');
+	log().info(vvr.toShortString());
 }
 
 function use(modName) {
 	var js = '';
 	if (modName == 'word') {
 		js = use_module('https://github.com/cogroid/c-jsb/raw/main/2022/06/04/003/word.js');
+	}
+	if (modName == 'variable') {
+		js = use_module('https://github.com/cogroid/c-jsb/raw/main/2022/06/04/003/variable.js');
 	}
 	var njs = js.replace('__exec__(data)', '__mod__' + modName + '()') + '';
 	return njs;
