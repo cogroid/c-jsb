16b0d295138054edbb226f506ad9bfae3a75c35c
cogroid <89304967+cogroid@users.noreply.github.com>
2022-06-07 14:01:38 -0400

    Update main.js

--- 2022/06/07/001/main.js
+++ 2022/06/07/001/main.js
@@ -1,7 +1,10 @@
 function __exec__(data) {
 	as().downloadFile('https://github.com/cogroid/c-jsb/raw/main/2022/06/07/001/OrderedLink.js', '/OrderedLink.js');
 	execJSB('/OrderedLink.js', data.input());  
-  
+
+	as().downloadFile('https://github.com/cogroid/c-jsb/raw/main/2022/06/07/001/OrderedLink.js', '/OrderedLink-2.js');
+	execJSB('/OrderedLink-2.js', data.input());  
+	
 	as().downloadFile('https://github.com/cogroid/c-jsb/raw/main/2022/06/07/001/OrderedLink.scm', '/OrderedLink.scm');
 	execSCM('/OrderedLink.scm', data.input());  
 }
