1964b98edcd0584b2c2e255ce6af388f473d06b6
cogroid <89304967+cogroid@users.noreply.github.com>
2022-06-07 13:43:22 -0400

    Create OrderedLink.scm

---
+++ 2022/06/07/001/OrderedLink.scm
@@ -0,0 +1,26 @@
+(display
+ (OrderedLink
+  (ConceptNode "dream")
+ )
+)
+(display
+ (OrderedLink
+  (ConceptNode "dream")
+  (ConceptNode "american")  
+ )
+)
+(display
+ (OrderedLink
+  (ConceptNode "dream")
+  (ConceptNode "american")  
+  (ConceptNode "freedom")  
+ )
+)
+(display
+ (OrderedLink
+  (ConceptNode "dream")
+  (ConceptNode "american")  
+  (ConceptNode "freedom")  
+  (ConceptNode "rich")  
+ )
+)
