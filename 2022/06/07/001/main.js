a8c26285b9b67ffa432d218a1f0efe2896fedfed
cogroid <89304967+cogroid@users.noreply.github.com>
2022-06-07 13:46:59 -0400

    Create main.js

---
+++ 2022/06/07/001/main.js
@@ -0,0 +1,18 @@
+function __exec__(data) {
+	as().downloadFile('https://github.com/cogroid/c-jsb/raw/main/2022/06/07/001/OrderedLink.js', '/OrderedLink.js');
+	execJSB('/OrderedLink.js', data.input());  
+  
+	as().downloadFile('https://github.com/cogroid/c-jsb/raw/main/2022/06/07/001/OrderedLink.scm', '/OrderedLink.scm');
+	execSCM('/OrderedLink.scm', data.input());  
+}
+
+function execSCM(scmFile, inputMap) {
+	as().scheme2js(scmFile, scmFile + '.js');
+	execJSB(scmFile + '.js', inputMap);	
+}
+
+function execJSB(jsFile, inputMap) {
+	log().info('===== ' + jsFile + ' =====');
+	var outputMap = mch().exec(jsFile, 60000, inputMap);
+	log().info('Result: ' + tool().toJson(outputMap));
+}
